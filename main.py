6b3eb346c47b479a96d20c0973e5c873be490b97
Clément <c.vier@students.ephec.be>
2024-12-14 21:15:38 +0100

    debut d'implem de main.py et ajout de raise dans import_CSV.py

---
+++ main.py
@@ -0,0 +1,13 @@
+from stock_manager import ImportCSV, SearchEngine, ReportGenerator
+import argparse
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-i', '--import', help='import CSV file')
+
+    main()
--- stock_manager/import_CSV.py
+++ stock_manager/import_CSV.py
@@ -1,6 +1,15 @@
 import pandas as pd
 
 
+class ColumnNameError(Exception):
+    pass
+
+class ColumnTypeError(Exception):
+    pass
+
+class EmptyCellError(Exception):
+    pass
+
 class ImportCSV:
     def __init__(self, csv_file, data_base):
         self.csv_file = pd.read_csv(csv_file, encoding='utf-8')
@@ -9,17 +18,14 @@ class ImportCSV:
 
     def _validity_check(self):
         if self.csv_file.columns.tolist() != self.data_base.columns.tolist(): # ['Nom du Produit', 'Quantité', 'Prix Unitaire', 'Catégorie']
-            return False, "Column names do not match."
+            raise ColumnNameError("Column names do not match.")
         if self.csv_file.dtypes.tolist() != self.data_base.dtypes.tolist():  # [dtype('O'), dtype('int64'), dtype('float64'), dtype('O')]
-            return False, "Column types do not match."
+            raise ColumnTypeError("Column types do not match.")
         if self.csv_file.isnull().values.any():
-            return False, "Empty cells found."
-        return True, "Valid CSV file."
+            raise EmptyCellError("Empty cells found.")
 
     def merge_data(self):
-        valid, message = self._validity_check()
-        if not valid:
-            return message
+        self._validity_check()
         data_merged = pd.concat([self.data_base, self.csv_file])
         column_order = self.data_base.columns.tolist()
         data_grouped =data_merged.groupby(['Nom du Produit', 'Catégorie'], as_index=False).agg({
@@ -28,4 +34,3 @@ class ImportCSV:
         })
         data_final = data_grouped[column_order]
         data_final.to_csv(self.data_base_address, index=False, encoding='utf-8')
-        return message + "\n Merged successfully."
