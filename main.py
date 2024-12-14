ef3293c96390d06b635acd0ff87ea9687eb800e3
Clément <c.vier@students.ephec.be>
2024-12-14 22:27:11 +0100

    paramètres dans le main

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from cgi import parse
+
 from stock_manager import ImportCSV, SearchEngine, ReportGenerator
 import argparse
 
@@ -8,6 +10,13 @@ def main():
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
-    parser.add_argument('-i', '--import', help='import CSV file')
+    parser.add_argument('data_base', help='The main csv file that contains your data.')
+    parser.add_argument('-i', '--import_csv', help='Import CSV file.')
+    parser.add_argument('-s', '--search', nargs='+', help='Do a research in the data base.')
+    parser.add_argument('-r', '--report', help='Create a report in the specified file.')
+    args = parser.parse_args()
+
+    if args.search and len(args.search) > 3:
+        parser.error("The --search option can maximum have three values.")
 
     main()
